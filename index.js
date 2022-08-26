adc28d9ae22735f9c8e2d6488cb9e7998c3f1cf1
Isaac Abrahamson <isaac.abrahamson@programmer.net>
2022-08-25 21:19:36 -0400

    add links

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // Initialize Wow.js
 new WOW({ mobile: false }).init()
 
+// Always top of page on reload
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+
 // Find heights of each box
 const innerBoxes = document.querySelectorAll('.innerBox')
 innerBoxes.forEach(innerBox => {
